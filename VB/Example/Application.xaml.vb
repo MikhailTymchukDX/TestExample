c479583c0948677cbb8d5c2991deb1c5afd54e20
Alexgoon <alex.russkovdx@gmail.com>
2017-02-22 10:03:26 +0300

    t=5 commit(VB)

---
+++ VB/Example/Application.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports System.Windows
+
+Namespace Example
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Partial Public Class App
+        Inherits Application
+
+        Private t As Integer=5
+    End Class
+End Namespace
